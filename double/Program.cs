8133a8f6654ff981ee6d4195b13cb47bf689d2e5
Selimology <kertekin2018@gmail.com>
2022-12-25 23:02:04 +0100

    Double

---
+++ double/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Doubles can have a decimal point or may be integer
+//Example: 1.23 , 43, 4,51, 351
+
+double averageAge;
+double notAverageAge;
+//If all values are integer, it will return integer.
+//If we want double, we can simply add a .0 to make it double
+averageAge = (43.0 + 22 + 62) / 3;
+
+
+notAverageAge = 42.3;
+
+System.Console.WriteLine(averageAge);
+System.Console.WriteLine(notAverageAge);
+
+//It is not recommended to use integer when dividing
+
+//Note, double also rounds the value up or down after 15digits.
+notAverageAge = 1.0/ 3;
+System.Console.WriteLine(notAverageAge);
+
