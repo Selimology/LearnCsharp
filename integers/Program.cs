53b17eb919769e0a04fea0ce49c2c04f0a229672
Selimology <kertekin2018@gmail.com>
2022-12-25 22:48:57 +0100

    Integers

---
+++ integers/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//If not assigned, it integer value is 0 by default.
+//below int is a signed
+int age = 0;
+//Int32 age = 0;
+
+age = 43;
+
+//For Unsigned 
+// UInt32 notage = 0;
+
+
+
+ 
+//signed Int32 upto  +/- two billion
+//unsigned  upto 4 billion
+
+//bit - 0 or 1
+//byte - 8 bits - 00000000
+
+
+
+int ageInTenYears = age + 10;
+
+System.Console.WriteLine(age);
+System.Console.WriteLine(ageInTenYears);
+
+// Will round it to integer if answer is decimal.
+System.Console.WriteLine(age / 2);
